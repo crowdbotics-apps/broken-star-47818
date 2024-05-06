abf88dd04c04a94331246c65b1eda6baf8561222
Crowdbotics <team@crowdbotics.com>
2024-05-06 17:57:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broken-star-47818",
+  url: "https://broken-star-47818.botics.co",
+  api: "https://broken-star-47818.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
